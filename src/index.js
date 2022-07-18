50eb064a8a942f1e419089100a3acdb0607bca18
Emma Repp <emmarepp.draws@gmail.com>
2022-07-18 11:48:06 -1000

    commit tree 6ef091b6b8e5d766afab25b8d117a25ecf87c7af

--- src/index.js
+++ src/index.js
@@ -13,3 +13,58 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const toysContainer = document.getElementById("toy-collection")
+const toysUrl = 'http://localhost:3000/toys'
+
+fetch(toysUrl)
+.then((respObj)=> respObj.json())
+.then( toysArray => {
+  toysArray.forEach( toyObj => {
+    const theDiv = document.createElement('div')
+    theDiv.className = 'card'
+
+    const nameElement = document.createElement('h2')
+   nameElement.textContent = toyObj.name
+
+   const imageElement = document.createElement('img')
+   imageElement.src = toyObj.image
+   imageElement.className = 'toy-avatar'
+
+   const likesElement = document.createElement('p')
+   likesElement.textContent = `${toyObj.likes} likes`
+
+   const likeBtn = document.createElement('button')
+   likeBtn.textContent = 'Like ❤️'
+   likeBtn.className = 'like-btn'
+   likeBtn.id = toyObj.id
+
+
+
+    theDiv.append(nameElement, imageElement, likesElement, likeBtn)
+    toysContainer.append(theDiv)
+    console.log( theDiv)
+  })
+  
+
+})
+const newToyForm = document.querySelector('form')
+  newToyForm.addEventlistener('submit', eventObj => {
+    eventObj.preventDefault()
+
+    const newToyObj = {
+      name: eventObj.target.name.value,
+      image: eventObj.target.image.value,
+      likes:0
+    }
+    fetch( toysUrl, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json' },
+      body:JSON.stringify (newToyObj)
+
+    })
+    .then( respObj => respObj.json() )
+    .then( aFreshNewToyObj => {
+      renderToy(aFreshNewToyObj)
+    })
+  })
